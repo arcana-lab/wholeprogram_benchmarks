57584c926dc2c61161bb113886fddbffa6049717
Brian Suchy (B$) <briansuchy2022@u.northwestern.edu>
2020-02-07 15:16:12 -0600

    Added cleaning functions to NAS, contemplating making these into Makefiles

--- build/NAS/rebuild.sh
+++ build/NAS/rebuild.sh
@@ -1,18 +1,24 @@
-rm -rf ${TOP_DIR}/bitcodes/LLVM9.0/NAS/*
-rm -rf ./NAS
-git clone https://github.com/benchmark-subsetting/NPB3.0-omp-C.git NAS
-cp make.def ./NAS/config/make.def
-cp suite.def ./NAS/config/suite.def
-cd NAS
-mkdir bin
-CC=gclang CXX=gclang++ make suite -j
-cd bin
+if [ $1 == "clean" ]
+then
+    rm -rf ./NAS
+else
+    rm -rf ${TOP_DIR}/bitcodes/LLVM9.0/NAS/*
+    rm -rf ./NAS
+    git clone https://github.com/benchmark-subsetting/NPB3.0-omp-C.git NAS
+    cp make.def ./NAS/config/make.def
+    cp suite.def ./NAS/config/suite.def
+    cd NAS
+    mkdir bin
+    CC=gclang CXX=gclang++ make suite -j
+    cd bin
 
-for file in ./*
-do
-    get-bc "$file"
-done
+    for file in ./*
+    do
+        get-bc "$file"
+    done
 
-mv ./*.bc ${TOP_DIR}/bitcodes/LLVM9.0/NAS/
-cd ${TOP_DIR}/bitcodes/LLVM9.0/NAS/
-tar -czf nas.tgz *
+    mv ./*.bc ${TOP_DIR}/bitcodes/LLVM9.0/NAS/
+    cd ${TOP_DIR}/bitcodes/LLVM9.0/NAS/
+    tar -czf nas.tgz *
+
+fi
