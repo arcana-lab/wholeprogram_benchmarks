338656b2ca01b3159a76243c05aa09c5ff338d39
Brian Suchy (B$) <briansuchy2022@u.northwestern.edu>
2020-02-07 15:13:08 -0600

    built base rebuild file for parsec

---
+++ build/parsec/rebuild.sh
@@ -0,0 +1,9 @@
+if [ $1 == "clean" ]
+then
+    rm -rf ./parsec-3.0*
+else
+rm -rf ${TOP_DIR}/bitcodes/LLVM9.0/parsec/*
+rm -rf ./parsec-3.0
+wget http://parsec.cs.princeton.edu/download/3.0/parsec-3.0-core.tar.gz
+tar xvf parsec-3.0-core.tar.gz
+fi
