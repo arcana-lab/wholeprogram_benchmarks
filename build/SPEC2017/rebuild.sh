efc682cce1c407e7737d8c990fc3981df4928b07
Vijay Kandiah <vkz4947@peroni.cs.northwestern.edu>
2020-02-09 01:55:28 -0600

    Added SPEC2017 to suite

--- build/SPEC2017/rebuild.sh
+++ build/SPEC2017/rebuild.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-rm -rf ${TOP_DIR}/bitcodes/LLVM9.0/SPEC2017/*
-rm -rf ./SPEC2017
+BUILD_DIR=`pwd`
+cd ${BUILD_DIR}
+
+
+if [ -d "${BUILD_DIR}/SPEC2017" ]; then
+  rm -rf ${BUILD_DIR}/SPEC2017
+fi
+if [ -d "${BUILD_DIR}/benchmarks/" ]; then
+  rm -rf ${BUILD_DIR}/benchmarks
+fi
+
+#extract new SPEC2017 Copy and patch
 tar -xf /project/benchmarks/SPEC2017.tar.gz
+
+cd SPEC2017
+chmod +x -R * #@Simone pls help
+printf 'yes' | ./install.sh  
+cp ${BUILD_DIR}/gclang.cfg config/
+cp ${BUILD_DIR}/pure_c_cpp_speed.bset benchspec/CPU/
+
+cd SPEC2017
+source shrc
+runcpu --loose -D --size test --tune peak -a setup --config gclang pure_c_cpp_speed
+
+echo "DONE installing SPEC2017 at ${BUILD_DIR}/SPEC2017" 
 exit
-chmod +x -R  
-cp gClang.cfg SPEC2017/config .
-cp make.def ./NAS/config/make.def
-cp suite.def ./NAS/config/suite.def
-cd NAS
-mkdir bin
-CC=gclang CXX=gclang++ make suite -j
-cd bin
 
-for file in ./*
-do
-    get-bc "$file"
-done
-
-mv ./*.bc ${TOP_DIR}/bitcodes/LLVM9.0/NAS/
-cd ${TOP_DIR}/bitcodes/LLVM9.0/NAS/
-tar -czf nas.tgz *
---
+++ build/SPEC2017/setupRun.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+if [ ! "${1}" == "test" ] && [ ! "${1}" == "train" ] && [ ! "${1}" == "refspeed" ]; then
+  echo "Please provide input configuration [test,train,refspeed] for setting up run directories. For Example: ./rebuild.sh refspeed "
+  exit
+fi
+
+
+source /project/gllvm/enable
+BUILD_DIR=`pwd`
+cd ../../bitcodes/LLVM9.0/SPEC2017
+BITCODE_DIR=`pwd` 
+cd ${BUILD_DIR}
+
+#delete previously generated bitcodes
+rm -rf ${BITCODE_DIR}/*
+
+if [ ! -d "${BUILD_DIR}/benchmarks/" ]; then
+  mkdir ${BUILD_DIR}/benchmarks
+fi
+
+
+#Setup Run directories with runcpu
+cd SPEC2017
+source shrc
+runcpu --loose --size ${1} --tune peak -a setup --config gclang pure_c_cpp_speed
+
+#Copy Run directories and extract bitcodes
+BENCHMARKS_DIR=${BUILD_DIR}/benchmarks
+
+for benchmark in omnetpp_s xalancbmk_s deepsjeng_s leela_s perlbench_s mcf_s lbm_s x264_s imagick_s nab_s xz_s; do
+	if [ ! -d "${BENCHMARKS_DIR}/${benchmark}" ]; then
+		mkdir ${BENCHMARKS_DIR}/${benchmark}
+	fi
+	if [ -d "${BENCHMARKS_DIR}/${benchmark}/$1" ]; then
+		rm -r ${BENCHMARKS_DIR}/${benchmark}/$1
+	fi
+	go ${benchmark} run
+	cp -r run_peak_${1}_gclang.0000 ${BENCHMARKS_DIR}/${benchmark}/$1
+	cd ${BENCHMARKS_DIR}/${benchmark}
+	mv $1/${benchmark}_peak.gclang ${BENCHMARKS_DIR}/${benchmark}/${benchmark}
+	get-bc ${benchmark}
+	cp ${benchmark}.bc ${BITCODE_DIR}/
+	
+	lastline="`tail -n 1 $1/speccmds.cmd`"
+	arguments="$( echo $lastline | awk -F'peak.gclang ' '{print $2}')"
+	echo '#!/bin/bash' > run_$1.sh
+	echo "cd `pwd`/$1" >> run_$1.sh
+	echo "./../${benchmark} ${arguments}" >> run_$1.sh
+	chmod +x run_$1.sh
+done
+echo "-----------------------------------------------------------"
+echo "Run directories created at ${BENCHMARKS_DIR} contain respective binaries and bitcodes. Run workload '${1}' with ./run.sh found at respective workload directories."  
+ 
+cd ${BITCODE_DIR}
+tar -czf spec2017.tgz *
+echo "DONE" 
+exit
+
