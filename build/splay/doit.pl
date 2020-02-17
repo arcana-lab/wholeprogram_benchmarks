e1a8780683c1833ed8dfb54bcfaeddfedb813f30
Brian Suchy (B$) <briansuchy2022@u.northwestern.edu>
2020-02-17 09:29:09 -0600

    Editted Makefile to be easier to instrument

--- build/splay/doit.pl
+++ build/splay/doit.pl
@@ -4,6 +4,8 @@ $#ARGV==2 or die "usage: doit.pl range_max num_keys num_lookups\n";
 
 ($range_max, $num_keys, $num_lookups) = @ARGV;
 
+
+
 $cmd = "./zipf_gen.pl $range_max $num_keys $num_lookups | ./run-splay $num_keys $num_lookups";
 
 system $cmd;
