2fb9e1b6e4ab594afe1ef5a9e77b06fbaa815993
Vijay Kandiah <vkz4947@peroni.cs.northwestern.edu>
2020-05-15 12:40:42 -0500

    updating parsec build script

--- build/parsec/rebuild.sh
+++ build/parsec/rebuild.sh
@@ -1,4 +1,9 @@
 rm -rf ${TOP_DIR}/bitcodes/LLVM9.0/parsec/*
 rm -rf ./parsec-3.0
+rm -rf *.gz
 wget http://parsec.cs.princeton.edu/download/3.0/parsec-3.0-core.tar.gz
+wget http://parsec.cs.princeton.edu/download/3.0/parsec-3.0-input-sim.tar.gz
+wget http://parsec.cs.princeton.edu/download/3.0/parsec-3.0-input-native.tar.gz
 tar xvf parsec-3.0-core.tar.gz
+tar xvf parsec-3.0-input-sim.tar.gz
+tar xvf parsec-3.0-input-native.tar.gz
