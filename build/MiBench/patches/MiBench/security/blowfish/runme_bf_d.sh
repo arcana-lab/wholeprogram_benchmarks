2eb21d253dae32c045fa730e81045b5bd6424388
Enrico Deiana <ead@u.northwestern.edu>
2020-06-23 13:10:38 -0500

    MiBench patches fixed.

---
+++ build/MiBench/patches/MiBench/security/blowfish/runme_bf_d.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./bf_d d output_large.enc output_large.asc 1234567890abcdeffedcba0987654321
---
+++ build/MiBench/patches/MiBench/security/blowfish/runme_bf_e.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./bf_e e input_large.asc output_large.enc 1234567890abcdeffedcba0987654321
