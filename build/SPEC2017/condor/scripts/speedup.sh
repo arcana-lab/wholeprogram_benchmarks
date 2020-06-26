e8261037666d9b5fc5f64b986396229f417e5184
Simone Campanoni <simo.xan@gmail.com>
2020-06-26 08:54:05 -0500

    Script: compute the median as final execution time

--- build/SPEC2017/condor/scripts/speedup.sh
+++ build/SPEC2017/condor/scripts/speedup.sh
@@ -4,6 +4,7 @@ benchmarks="`ls benchmarks`" ;
 finalOutput="" ;
 
 # Setup
+numRuns="5" ;
 rm -f noelle_speedup.txt ;
 touch baseline_times.txt ;
 touch noelle_times.txt ;
@@ -27,14 +28,37 @@ for b in $benchmarks ; do
     make binary BENCHMARK=$b ;
 
     # Run the baseline
-    make run BENCHMARK=$b INPUT=train &> baseline_$b.txt ;
-    baselineTime=`awk '{
-        if (  ($2 == "seconds") &&
-              ($3 == "time")    &&
-              ($4 == "elapsed") ){
-          print $1 ;
+    touch baseline_${b}.txt ;
+    for i in `seq 1 $numRuns` ; do
+
+      # Run
+      make run BENCHMARK=$b INPUT=train &> baseline_${b}_${i}.txt ;
+
+      # Collect the time
+      baselineTime=`awk '{
+          if (  ($2 == "seconds") &&
+                ($3 == "time")    &&
+                ($4 == "elapsed") ){
+            print $1 ;
+          }
+        }' baseline_${b}_${i}.txt `;
+
+      # Append the time
+      echo "$baselineTime" >> baseline_${b}.txt ;
+    done
+
+    # Fetch the median
+    baselineTime=`awk -v n=$numRuns '
+      BEGIN {
+        l = 0;
+        target = n/2 ;
+      } {
+        if (l >= target){
+          print ;
+          exit ;
         }
-      }' baseline_$b.txt `;
+        l++ ;
+      }' baseline_${b}.txt` ;
     echo "$b $baselineTime" >> baseline_times.txt ;
   fi
   echo "  Baseline: $baselineTime" ;
@@ -62,18 +86,46 @@ for b in $benchmarks ; do
     fi
 
     # Run the parallelized binary
-    make run BENCHMARK=$b INPUT=train &> noelle_$b.txt ;
-    if test $? -ne 0 ; then
-      echo "$b Error during execution of the parallel binary" >> noelle_speedup.txt ;
+    hasFailed="0" ;
+    touch noelle_${b}.txt ;
+    for i in `seq 1 $numRuns` ; do
+
+      # Run
+      make run BENCHMARK=$b INPUT=train &> noelle_${b}_${i}.txt ;
+      if test $? -ne 0 ; then
+        echo "$b Error during execution of the parallel binary" >> noelle_speedup.txt ;
+        hasFailed="1" ;
+        break ;
+      fi
+
+      # Collect the time
+      noelleTime=`awk '{
+          if (  ($2 == "seconds") &&
+                ($3 == "time")    &&
+                ($4 == "elapsed") ){
+            print $1 ;
+          }
+        }' noelle_${b}_${i}.txt `;
+
+      # Append the time
+      echo "$noelleTime" >> noelle_${b}.txt ;
+    done
+    if test "$hasFailed" == "1" ; then
       continue ;
     fi
-    noelleTime=`awk '{
-        if (  ($2 == "seconds") &&
-              ($3 == "time")    &&
-              ($4 == "elapsed") ){
-          print $1 ;
+
+    # Fetch the median
+    noelleTime=`awk -v n=$numRuns '
+      BEGIN {
+        l = 0;
+        target = n/2 ;
+      } {
+        if (l >= target){
+          print ;
+          exit ;
         }
-      }' noelle_$b.txt `;
+        l++ ;
+      }' noelle_${b}.txt` ;
     echo "$b $noelleTime" >> noelle_times.txt ;
   fi
   echo "  Parallelized: $noelleTime" ;
