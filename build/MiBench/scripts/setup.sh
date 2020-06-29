9b87ed92cc75dc39396eb7b2733b7afe455409fe
Enrico Deiana <ead@u.northwestern.edu>
2020-06-29 16:05:40 -0500

    New bitcode_copy.sh script, bug fixes for the rest of MiBench scripts.

--- build/MiBench/scripts/setup.sh
+++ build/MiBench/scripts/setup.sh
@@ -4,19 +4,4 @@
 PWD_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.." ;
 benchmarkSuiteName="MiBench" ;
 
-# Get a list of all generated binaries, we'll use it to extract bitcode files
-llvmVersion=`llvm-config --version` ;
-bitcodesDir="${PWD_PATH}/../../bitcodes/LLVM${llvmVersion}/${benchmarkSuiteName}" ;
-
-# Check if bitcode dir exists
-if ! test -d ${bitcodesDir} ; then
-  echo "ERROR: ${bitcodesDir} not found. Run make bitcode." ;
-  exit 1 ;
-fi
-
-# Get bitcode benchmark dir
-benchmarksDir="${PWD_PATH}/benchmarks" ;
-mkdir -p ${benchmarksDir} ;
-
-# Copy bitcode and path.txt for every benchmark
-cp -r ${bitcodesDir}/* ${benchmarksDir} ;
+echo "DONE." ;
