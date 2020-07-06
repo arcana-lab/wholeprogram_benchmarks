7598db24c8139a63b742cc758c60ba54eb3bcb1d
Enrico Deiana <ead@u.northwestern.edu>
2020-07-06 12:56:35 -0500

    New bitcode_copy rule for SPEC2017, so we don;t need to create new bitcodes every time.

---
+++ build/SPEC2017/scripts/bitcode_copy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Get benchmark suite dir
+PWD_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.." ;
+benchmarkSuiteName="SPEC2017" ;
+
+# Get a list of all generated binaries, we'll use it to extract bitcode files
+llvmVersion=`llvm-config --version` ;
+bitcodesDir="${PWD_PATH}/../../bitcodes/LLVM9.0/${benchmarkSuiteName}" ;
+
+# Check if bitcode dir exists
+if ! test -d ${bitcodesDir} ; then
+  echo "ERROR: ${bitcodesDir} not found. Run make bitcode." ;
+  exit 1 ;
+fi
+
+# Get bitcode benchmark dir
+benchmarksDir="${PWD_PATH}/benchmarks" ;
+mkdir -p ${benchmarksDir} ;
+
+# Copy bitcode in benchmark
+cp -r ${bitcodesDir}/* ${benchmarksDir}/ ;
