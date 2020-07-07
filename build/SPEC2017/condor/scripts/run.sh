1305eacbcbd18d3e02fe4d460a7561689b87d5ae
Simone Campanoni <simo.xan@gmail.com>
2020-07-07 16:52:45 -0500

    SPEC: added scripts to run binaries via condor

---
+++ build/SPEC2017/condor/scripts/run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Fetch the input
+if test $# -lt 3 ; then
+  echo "USAGE: `basename $0` BENCHMARK RUNS OUTPUT_FILE" ;
+  exit 1;
+fi
+b="$1" ;
+numRuns=$2 ;
+outputFile="$3" ;
+
+# Create a temporary file
+tempFile=`mktemp` ;
+
+# Run 
+echo "Running ${b}" ;
+touch ${outputFile} ;
+for i in `seq 1 $numRuns` ; do
+
+  # Run
+  make run BENCHMARK=$b INPUT=train &> ${tempFile} ;
+  if test $? -ne 0 ; then
+    echo "$b Error during binary generation" >> ${outputFile} ;
+    break ;
+  fi
+
+  # Collect the time
+  baselineTime=`awk '{
+      if (  ($2 == "seconds") &&
+            ($3 == "time")    &&
+            ($4 == "elapsed") ){
+        print $1 ;
+      }
+    }' ${tempFile}` ;
+
+  # Append the time
+  echo "$baselineTime" >> ${outputFile} ;
+
+done
+
+# Clean
+rm $tempFile ;
+
+exit 0;
