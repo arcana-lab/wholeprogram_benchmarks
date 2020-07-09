4b39a7badf03c71d7e1203a5befbb7b5f69fd2a9
Simone Campanoni <simo.xan@gmail.com>
2020-07-09 16:58:55 -0500

    SPEC: improved the cleaning

--- build/SPEC2017/condor/scripts/clean.sh
+++ build/SPEC2017/condor/scripts/clean.sh
@@ -5,3 +5,6 @@ rm -f benchmarks/*/default.profraw ;
 rm -f benchmarks/*/baseline* ;
 rm -f benchmarks/*/noelle_output.txt ;
 rm -f benchmarks/*/*train_output.txt ;
+rm -f benchmarks/*/NOELLE_input.bc ;
+rm -f benchmarks/*/*newbin;
+
