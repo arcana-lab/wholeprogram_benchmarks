b7fddb0631984137910d9951ca661a2d4653d915
Simone Campanoni <simo.xan@gmail.com>
2020-07-13 16:31:12 -0500

    Spec: added script to compute speedups

---
+++ build/SPEC2017/condor/scripts/compareTimes.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+function computeMedian {
+  local dataFile=$1 ;
+  local tempFile=`mktemp` ;
+
+  # Sort the data
+  sort -g $dataFile > $tempFile ;
+
+  # Fetch the number of lines of the file
+  lines=`wc -l $tempFile | awk '{print $1}'` ;
+
+  # Compute the median
+  median=`echo "$lines / 2" | bc` ;
+
+  # Fetch the median
+  echo $median
+  result=`awk -v median=$median '
+    BEGIN {
+      c = 0;
+    }{
+      if (c == median){
+        print ;
+      }
+      c++;
+    }' $tempFile` ;
+
+  # Free
+  rm $tempFile ;
+
+  return ;
+}
+
+# Fetch the inputs
+if test $# -lt 3 ; then
+  echo "USAGE: `basename $0` BASELINE_DIRECTORY OPTIMIZATION_DIRECTORY SPEEDUP_FILE" ;
+  echo "" ;
+  echo "Each directory needs to have a subdirectory called \"data\" where all text files are" ;
+  exit 1;
+fi
+baselineDir="`realpath $1`" ;
+optDir="`realpath $2`" ;
+speedupFile="`realpath $3`" ;
+
+# Variables
+origDir="`pwd`" ;
+result="0" ;
+
+# Compute the speedups
+pushd ./ ;
+cd $optDir ;
+cd data ;
+for i in `ls` ; do
+
+  # Compute the name of the benchmark
+  bench=`echo "$i" | sed s/times_//g | sed s/\.txt//g` ;
+  echo "Benchmark: $i" ;
+
+  # Fetch the files
+  optFile="`realpath $i`" ;
+  baselineFile="`realpath ${baselineDir}/data/$i`" ;
+  echo "  Baseline = $baselineFile" ;
+  echo "  Optimization = $optFile" ;
+  if ! test -f $baselineFile ; then
+    echo "  The baseline file doesn't exist" ;
+    continue ;
+  fi
+
+  # Fetch the medians
+  computeMedian $baselineFile ;
+  baselineMedian=$result ;
+  echo "  Baseline = $baselineMedian" ;
+
+  computeMedian $optFile ;
+  optMedian=$result ;
+  echo "  Baseline = $optMedian" ;
+
+  # Compute the speedup
+  speedup=`echo "scale=3; $baselineMedian / $optMedian" | bc` ;
+  echo "  Speedup = $speedup" ;
+
+  # Dump the speedup
+  echo "$bench $speedup" >> $speedupFile ;
+done
+popd ;
