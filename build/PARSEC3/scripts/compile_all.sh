3a6a230844b5db3692379d5769992ca4e518e449
Enrico Deiana <ead@u.northwestern.edu>
2020-07-19 03:45:21 -0500

    New scripts for parsec benchmarks.

---
+++ build/PARSEC3/scripts/compile_all.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Setup the local variables and environment
+BUILD_DIR=`pwd`
+pushd ./;
+cd ../../install/bin ;
+export PATH=`pwd`:$PATH ;
+popd ;
+
+# Check if gclang is in PATH
+which gclang ;
+if [ "$?" != "0" ] ; then
+  echo "ERROR: gclang is not in PATH. For Zythos cluster users, execute: export PATH=/project/gllvm/gllvm/bin:$\{PATH\}" ;
+  exit 1 ;
+fi
+
+# Get benchmark suite dir
+PWD_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.." ;
+benchmarkSuiteName="parsec-3.0" ;
+
+# Check benchmark suite is extracted
+pathToBenchmarkSuite="${PWD_PATH}/${benchmarkSuiteName}" ;
+if ! test -d ${pathToBenchmarkSuite} ; then
+  echo "ERROR: ${pathToBenchmarkSuite} not found. Run make install." ;
+  exit 1 ;
+fi
+
+# Error file: contains list of failing benchmarks
+errorFile="${PWD_PATH}/error_compiling.txt" ;
+rm -f ${errorFile} ;
+
+# Go inside benchmark suite dir
+cd ${pathToBenchmarkSuite} ;
+
+# Clean up environment
+./bin/parsecmgmt -a fulluninstall -c gclang ;
+./bin/parsecmgmt -a fullclean -c gclang ;
+
+# Compile list of benchmarks with gclang (cannot compile them all, parsec stops at the first error)
+benchmarksList=("blackscholes" "bodytrack" "fluidanimate" "freqmine" "swaptions" "x264" "canneal" "streamcluster") ;
+for elem in ${benchmarksList[@]} ; do
+  ./bin/parsecmgmt -a build -c gclang -p ${elem} ;
+done
---
+++ build/PARSEC3/scripts/optimization.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+function runOptimizations {
+  # Check for benchmark dir
+  if [ ! -d "${benchmarksDir}/${1}" ]; then
+    echo "Warning: Benchmark directory ${1} not found, skipping." ;
+    return ;
+  fi
+
+  # Go in the benchmark dir
+	cd ${benchmarksDir}/${1} ;
+
+  # Copy run script for default.profraw execution
+  cp ${PWD_PATH}/makefiles/run.sh . ;
+
+  # Run optimization
+	echo "Running your optimizations for \"${1}\"" ;
+	cp ${PWD_PATH}/makefiles/* . ;
+	make clean > /dev/null ;
+  timeout 30m make BENCHMARK=${1} >> noelle_output.txt 2>&1 ;
+  exitOutput=$? ;
+  return ;
+}
+
+# Get benchmark suite dir
+PWD_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.." ;
+
+# Get bitcode benchmark dir
+benchmarksDir="${PWD_PATH}/benchmarks" ;
+if ! test -d ${benchmarksDir} ; then
+  echo "ERROR: ${benchmarksDir} not found. Run make setup." ;
+  exit 1 ;
+fi
+
+# Run optimizations on all benchmarks
+if [ "${1}" == "all" ]; then
+	for benchmark in `ls ${benchmarksDir}`; do
+		runOptimizations ${benchmark} ;
+	done
+
+# Run optimizations for the specified benchmark name
+else
+	runOptimizations ${1} ;
+fi
+
+echo "-----------------------------------------------------------"
+echo "DONE"
+
+exit ${exitOutput} ;
