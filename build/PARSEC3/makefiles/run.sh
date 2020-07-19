0d098be83b84bbc0bc9366232d0f1413a953651b
Enrico Deiana <ead@u.northwestern.edu>
2020-07-19 03:46:21 -0500

    Added NOELLE makefile stuff for parsec, and run.sh to run baseline.

---
+++ build/PARSEC3/makefiles/run.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+function runBenchmark {
+  # Get function args
+  benchmarkArg="${1}" ;
+  binaryNameArg="${2}" ;
+
+  inputArg="simlarge" ;
+
+  # Check if paths exists
+  pathToBenchmark="${PWD_PATH}/benchmarks/${benchmarkArg}" ;
+  if ! test -d ${pathToBenchmark} ; then
+    echo "WARNING: ${pathToBenchmark} not found. Skipping..." ;
+    return ;
+  fi
+
+  pathToBinaryPath="${pathToBenchmark}/path.txt" ;
+  if ! test -f ${pathToBinaryPath} ; then
+    echo "WARNING: ${pathToBinaryPath} not found. Skipping..." ;
+    return ;
+  fi
+
+  pathToBinary=`cat ${pathToBinaryPath}` ;
+  if ! test -d ${pathToBinary} ; then
+    echo "WARNING: ${pathToBinary} not found. Skipping..." ;
+    return ;
+  fi
+
+  currBinary="${pathToBenchmark}/${binaryNameArg}" ;
+  if ! test -f ${currBinary} ; then
+    echo "WARNING: ${currBinary} not found. Skipping..." ;
+    return ;
+  fi
+
+  pathToInputConf="${pathToBinary}/../../../parsec/${inputArg}.runconf" ;
+  if ! test -f ${pathToInputConf} ; then
+    echo "WARNING: ${pathToInputConf} not found. Skipping..." ;
+    return ;
+  fi
+
+  # Go in the benchmark dir
+  cd ${pathToBenchmark} ;
+
+  # Create run dir
+  rm -rf ./run ;
+  mkdir ./run ;
+
+  # Copy binary into run dir under benchmark
+  cp ${currBinary} ./run ;
+
+  # Extract inputs in run dir if the input archive exists
+  cd ./run ;
+  pathToBenchmarkInput="${pathToBinary}/../../../inputs/input_${inputArg}.tar" ;
+  if test -f ${pathToBenchmarkInput} ; then
+    tar xf ${pathToBinary}/../../../inputs/input_${inputArg}.tar ;
+  fi
+
+  # Get args to run binary with
+  NTHREADS=1 source ${pathToInputConf} ;
+
+  # Run benchmark in benchmarks/${benchmark}/run dir
+  commandToRunSplit="./${binaryNameArg} ${run_args}" ;
+  echo "Running: ${commandToRunSplit} in ${PWD}" ;
+  eval ${commandToRunSplit} ;
+  if [ "$?" != 0 ] ; then
+    echo "ERROR: run of ${commandToRunSplit} failed." ;
+    return ;
+  fi
+
+	echo "--------------------------------------------------------------------------------------" ;
+
+  return ;
+}
+
+# Get benchmark suite dir
+PWD_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.." ;
+
+# Get args
+benchmarkToRun="${1}" ;
+binaryName="${2}" ;
+
+# Get bitcode benchmark dir
+benchmarksDir="${PWD_PATH}/benchmarks" ;
+if ! test -d ${benchmarksDir} ; then
+  echo "ERROR: ${benchmarksDir} not found. Run make setup." ;
+  exit 1 ;
+fi
+
+# Run benchmark
+if [ "${benchmarkToRun}" == "all" ]; then
+	for benchmark in `ls ${benchmarksDir}`; do
+    runBenchmark ${benchmark} ${binaryName} ;
+	done
+else
+  runBenchmark ${benchmarkToRun} ${binaryName} ;
+fi
+
+echo "DONE" 
+exit
