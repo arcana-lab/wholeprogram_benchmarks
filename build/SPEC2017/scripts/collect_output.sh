873106c01c68497c37ed7be4f77fb853ba31cc1a
Simone Campanoni <simo.xan@gmail.com>
2020-08-16 17:08:58 -0500

    Scripts: improved the collection of output files

--- build/SPEC2017/scripts/collect_output.sh
+++ build/SPEC2017/scripts/collect_output.sh
@@ -5,21 +5,41 @@ if test $# -lt 1 ; then
   echo "USAGE: `basename $0` OUTPUT_DIR" ;
   exit 1 ;
 fi
-outDir="$1" ;
+filesDir="$1" ;
 
 # Create the output directory
+outDir="${filesDir}/output" ;
 mkdir -p ${outDir} ;
 
-# Copy the outputs
+# Create the bitcode directory
+bitcodeDir="${filesDir}/parallelized_bitcode" ;
+mkdir -p ${bitcodeDir} ;
+
+# Copy the text outputs ;
+cp *.txt ${filesDir}/ ;
+
+# Copy the makefile
+cp makefiles/Makefile ${filesDir}/ ;
+
+# Copy the files ;
 for i in `find ./benchmarks -name noelle_output.txt` ; do
   echo "Copy $i" ;
 
-  # Create the benchmark directory in the output directory
+  # Fetch the names
   benchDir=`dirname $i` ;
+  benchName=`basename $benchDir` ;
+  fileName=`basename $i` ;
+
+  # Create the benchmark directory in the output directory
   mkdir -p ${outDir}/$benchDir ;
 
   # Copy the output
-  fileName=`basename $i` ;
   cp $i ${outDir}/${benchDir}/$fileName ;
 
+  # Copy the parallelized bitcode
+  bitcodeFile="${benchDir}/${benchName}.bc" ;
+  if test -f $bitcodeFile ; then
+    mkdir -p ${bitcodeDir}/${benchDir} ;
+    cp ${bitcodeFile} ${bitcodeDir}/${benchDir}/ ;
+  fi
 done
