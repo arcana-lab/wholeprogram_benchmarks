8d7d216055cce2aa4823879e9d9103c1ff42cd5d
Enrico Deiana <ead@u.northwestern.edu>
2020-12-08 14:57:50 -0600

    SPEC2017 bug fix in install.sh

--- build/SPEC2017/scripts/install.sh
+++ build/SPEC2017/scripts/install.sh
@@ -6,7 +6,6 @@ if [ -d "${PWD_PATH}/benchmarks/" ]; then
   rm -rf ${PWD_PATH}/benchmarks ;
 fi
 
-cp ${PWD_PATH}/patches/gclang.cfg ${PWD_PATH}/SPEC2017/config/ ;
 cp ${PWD_PATH}/patches/pure_c_cpp_speed.bset ${PWD_PATH}/SPEC2017/benchspec/CPU/ ;
 cp ${PWD_PATH}/patches/pure_c_cpp_rate.bset ${PWD_PATH}/SPEC2017/benchspec/CPU/ ;
 
@@ -17,4 +16,5 @@ cp -r ${PWD_PATH}/patches/SPEC2017 ${PWD_PATH} ;
 
 cd SPEC2017 ;
 printf 'yes' | ./install.sh ;
+cp ${PWD_PATH}/patches/gclang.cfg ${PWD_PATH}/SPEC2017/config/ ;
 
