aa86fb1fd146ad30339df3eb72f98ef4f78ae53d
Enrico Deiana <ead@u.northwestern.edu>
2020-12-08 15:00:34 -0600

    install.sh fixed

--- build/SPEC2017/scripts/install.sh
+++ build/SPEC2017/scripts/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 PWD_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.." ;
 
@@ -6,6 +6,9 @@ if [ -d "${PWD_PATH}/benchmarks/" ]; then
   rm -rf ${PWD_PATH}/benchmarks ;
 fi
 
+cd SPEC2017 ;
+printf 'yes' | ./install.sh ;
+cp ${PWD_PATH}/patches/gclang.cfg ${PWD_PATH}/SPEC2017/config/ ;
 cp ${PWD_PATH}/patches/pure_c_cpp_speed.bset ${PWD_PATH}/SPEC2017/benchspec/CPU/ ;
 cp ${PWD_PATH}/patches/pure_c_cpp_rate.bset ${PWD_PATH}/SPEC2017/benchspec/CPU/ ;
 
@@ -13,8 +16,4 @@ cd ${PWD_PATH}/SPEC2017 ;
 tar xf ${PWD_PATH}/patches/parest_r_patch.tar.xz ;
 
 cp -r ${PWD_PATH}/patches/SPEC2017 ${PWD_PATH} ;
-
-cd SPEC2017 ;
-printf 'yes' | ./install.sh ;
-cp ${PWD_PATH}/patches/gclang.cfg ${PWD_PATH}/SPEC2017/config/ ;
 
