c916d876deb438f3e71b3befb769dc637d4e3336
Enrico Deiana <ead@u.northwestern.edu>
2020-12-08 14:42:32 -0600

    Bug fix in install and uninstall scripts for SPEC2017

--- build/SPEC2017/scripts/install.sh
+++ build/SPEC2017/scripts/install.sh
@@ -2,18 +2,19 @@
 
 PWD_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.." ;
 
-BUILD_DIR=`pwd`
-cd ${BUILD_DIR}
-
-if [ -d "${BUILD_DIR}/benchmarks/" ]; then
-  rm -rf ${BUILD_DIR}/benchmarks
+if [ -d "${PWD_PATH}/benchmarks/" ]; then
+  rm -rf ${PWD_PATH}/benchmarks ;
 fi
 
-cd SPEC2017
-printf 'yes' | ./install.sh  
-cp ${BUILD_DIR}/patches/gclang.cfg config/
-cp ${BUILD_DIR}/patches/pure_c_cpp_speed.bset benchspec/CPU/
-cp ${BUILD_DIR}/patches/pure_c_cpp_rate.bset benchspec/CPU/
-tar xf ${BUILD_DIR}/patches/parest_r_patch.tar.xz
+cp ${PWD_PATH}/patches/gclang.cfg ${PWD_PATH}/SPEC2017/config/ ;
+cp ${BUILD_DIR}/patches/pure_c_cpp_speed.bset ${PWD_PATH}/SPEC2017/benchspec/CPU/ ;
+cp ${BUILD_DIR}/patches/pure_c_cpp_rate.bset ${PWD_PATH}/SPEC2017/benchspec/CPU/ ;
+
+cd ${PWD_PATH}/SPEC2017 ;
+tar xf ${BUILD_DIR}/patches/parest_r_patch.tar.xz ;
 
 cp -r ${PWD_PATH}/patches/SPEC2017 ${PWD_PATH} ;
+
+cd SPEC2017 ;
+printf 'yes' | ./install.sh ;
+
