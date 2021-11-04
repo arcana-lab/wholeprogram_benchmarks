eff54c14b3989ec3650ec443f44a29c7682f44cd
Simone Campanoni <simo.xan@gmail.com>
2021-11-03 19:20:48 -0500

    MiBench: fixed bf_d and bf_e

--- build/MiBench/patches/MiBench/security/blowfish/runme_bf_d.sh
+++ build/MiBench/patches/MiBench/security/blowfish/runme_bf_d.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-./bf_d d input_verylarge.enc output_verylarge.asc 1234567890abcdeffedcba0987654321
+fileInput="input_verylarge.enc";
+if ! test -f $fileInput ; then
+  wget --no-check-certificate http://users.cs.northwestern.edu/~simonec/files/Software/MiBench/${fileInput}.xz ;
+  tar -d ${fileInput}.xz ;
+fi
+
+./bf_d d $fileInput output_verylarge.asc 1234567890abcdeffedcba0987654321
--- build/MiBench/patches/MiBench/security/blowfish/runme_bf_e.sh
+++ build/MiBench/patches/MiBench/security/blowfish/runme_bf_e.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+fileInput="input_verylarge.asc";
+if ! test -f $fileInput ; then
+  tar -d ${fileInput}.xz ;
+fi
+
 ./bf_e e input_verylarge.asc output_verylarge.enc 1234567890abcdeffedcba0987654321
