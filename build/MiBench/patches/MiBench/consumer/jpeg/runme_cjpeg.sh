79a0bccf5fcf64dd82b965041d27320a7537c91e
Enrico Deiana <ead@u.northwestern.edu>
2023-04-24 18:11:08 -0500

    Fixed generation of binaries in MiBench. All binaries that are executed are now directly inside the benchmarks dir so that when NOELLE runs make binary ; make run the binaries executed by runme_BENCHMARK.sh are those generated by NOELLE and not the default ones.

--- build/MiBench/patches/MiBench/consumer/jpeg/runme_cjpeg.sh
+++ build/MiBench/patches/MiBench/consumer/jpeg/runme_cjpeg.sh
@@ -2,4 +2,4 @@
 
 ./unpack_input.sh "input_very_large.ppm";
 
-./jpeg-6a/cjpeg -dct int -progressive -opt -outfile output_very_large_encode.jpeg input_very_large.ppm ;
+./cjpeg -dct int -progressive -opt -outfile output_very_large_encode.jpeg input_very_large.ppm ;
--- build/MiBench/patches/MiBench/consumer/jpeg/runme_djpeg.sh
+++ build/MiBench/patches/MiBench/consumer/jpeg/runme_djpeg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./jpeg-6a/djpeg -dct int -ppm -outfile output_very_large_decode.ppm input_very_large.jpeg
+./djpeg -dct int -ppm -outfile output_very_large_decode.ppm input_very_large.jpeg
