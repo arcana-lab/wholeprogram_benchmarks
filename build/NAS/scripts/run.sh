6769dbfca00c0ff010f9582b4969f28c034b5dd9
Enrico Deiana <ead@u.northwestern.edu>
2023-04-26 13:56:00 -0500

    Added generation of input file containing arguments to run benchmarks in NAS.

--- build/NAS/scripts/run.sh
+++ build/NAS/scripts/run.sh
@@ -1,82 +1,31 @@
 #!/bin/bash
 
-function split {
-  headOrTail="${1}" ;
-  stringToSplit="${2}" ;
-  IFS=' ' ;
-  read -a stringAsArray <<< "${stringToSplit}" ;
-
-  if [ ${headOrTail} == "h" ] ; then
-    echo ${stringAsArray[0]} ;
-  else
-    echo ${stringAsArray[@]:1} ;
-  fi
-
-  return ;
-}
-
 function runBenchmark {
   # Get function args
   benchmarkArg="${1}" ;
 
   # Check if paths exists
-  pathToBenchmark="${PWD_PATH}/benchmarks/${benchmarkArg}" ;
+  pathToBenchmark="${benchmarksDir}/${benchmarkArg}" ;
   if ! test -d ${pathToBenchmark} ; then
     echo "WARNING: ${pathToBenchmark} not found. Skipping..." ;
     return ;
   fi
 
-  pathToBinaryPath="${pathToBenchmark}/path.txt" ;
-  if ! test -f ${pathToBinaryPath} ; then
-    echo "WARNING: ${pathToBinaryPath} not found. Skipping..." ;
-    return ;
-  fi
-
-  pathToBinary=`cat ${pathToBinaryPath}` ;
-  if ! test -d ${pathToBinary} ; then
-    echo "WARNING: ${pathToBinary} not found. Skipping..." ;
-    return ;
-  fi
-
-  currBinary="${pathToBenchmark}/${benchmarkArg}" ;
-  if ! test -f ${currBinary} ; then
-    echo "WARNING: ${currBinary} not found. Skipping..." ;
-    return ;
-  fi
-
-  # Go in the benchmark suite where the script is
-  cd ${pathToBinary} ;
-
-  # Get arguments of how to run the binary
-#  runScript="./runme_${benchmarkArg}.sh" ;
-#  if ! test -f ${runScript} ; then
-#    echo "WARNING: ${runScript} not found. Going up one dir." ;
-#    cd ../ ;
-#    if ! test -f ${runScript} ; then
-#      echo "WARNING: ${runScript} not found. Skipping..." ;
-#      return ;
-#    fi
-#  fi
-
-  # Unpack the input if necessary
-#  if test -f ./unpack_input.sh ; then
-#    ./unpack_input.sh ;
-#  fi
+  # Go back in the benchmark dir
+  cd ${pathToBenchmark} ;
 
   # Set the stack size to be unlimited. This is needed for IS
   ulimit -s unlimited  ;
 
   # Run the benchmark
-#  commandToRun=`tail -n 1 ${runScript}` ;
-  binary=./${benchmarkArg} ; #$(split h "${commandToRun}") ;
+  binary="./${benchmarkArg}" ;
 
   # Copy binary into benchmark suite (i.e., current directory)
-  echo "Executing ${pathToBinary}/${benchmarkArg}" ;
-  cp ${currBinary} . ;
+  echo "Executing ${benchmarkArg}" ;
 
-  perfStatFile="${PWD_PATH}/benchmarks/${benchmarkArg}/${benchmarkArg}_large_output.txt" ;
+  perfStatFile="${pathToBenchmark}/${benchmarkArg}_large_output.txt" ;
   commandToRunSplit="${binary}" ;
-  echo "Running: ${commandToRunSplit} in ${PWD}" ;
+  echo "Running: ${commandToRunSplit} in ${pathToBenchmark}" ;
   # eval perf stat ${commandToRunSplit} > ${perfStatFile} ;
   eval /usr/bin/time -p ${commandToRunSplit} 2>&1 > ${perfStatFile} ;
   if [ "$?" != 0 ] ; then
--- build/NAS/scripts/setup.sh
+++ build/NAS/scripts/setup.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
 
+function genInputBenchmark {
+  # Get function args
+  benchmarkArg="${1}" ;
+
+  # Check if paths exists
+  pathToBenchmark="${benchmarksDir}/${benchmarkArg}" ;
+  if ! test -d ${pathToBenchmark} ; then
+    echo "WARNING: ${pathToBenchmark} not found. Skipping..." ;
+    return ;
+  fi
+
+  # Go in the benchmark suite where the script is
+  cd ${pathToBenchmark} ;
+
+  # Create autotuner input file
+  touch autotuner_input.txt ;
+
+  return ;
+}
+
 # Get benchmark suite dir
 PWD_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.." ;
-benchmarkSuiteName="NAS" ;
+
+# Get args
+benchmarkToRun="${1}" ;
+
+# Get bitcode benchmark dir
+benchmarksDir="${PWD_PATH}/benchmarks" ;
+if ! test -d ${benchmarksDir} ; then
+  echo "ERROR: ${benchmarksDir} not found. Run make bitcode_copy." ;
+  exit 1 ;
+fi
+
+# Generate input file that contains the arguments to run the benchmark later
+if [ "${benchmarkToRun}" == "all" ]; then
+	for benchmark in `ls ${benchmarksDir}`; do
+    genInputBenchmark ${benchmark} ;
+	done
+else
+  genInputBenchmark ${benchmarkToRun} ;
+fi
 
 echo "DONE." ;
