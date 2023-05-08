915f33011b30ef83548a2b906e6280fb5e431afc
Nick Wanninger <nickwanninger@users.noreply.github.com>
2023-05-08 10:25:58 -0500

    Mibench/qsort: cleanup code

--- build/MiBench/patches/MiBench/automotive/qsort/qsort_large.c
+++ build/MiBench/patches/MiBench/automotive/qsort/qsort_large.c
@@ -1,15 +1,16 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define UNLIMIT
-#define MAXARRAY 999999999 // ED: was 60000 /* this number, if too large, will cause a seg. fault!! */
+#define MAXARRAY                                                               \
+  999999999 // ED: was 60000 /* this number, if too large, will cause a seg.
+            // fault!! */
 
 struct my3DVertexStruct {
   int x, y, z;
   double distance;
 };
-
 typedef struct my3DVertexStruct vertex_t;
 
 int vertex_compare(const vertex_t *restrict elem1,
@@ -32,6 +33,7 @@ void quicksort(off_t bottom, off_t top, vertex_t *restrict data) {
     return;
 
   vertex_t *pivot = &data[bottom];
+
   for (lower = bottom, upper = top; lower < upper;) {
     while (lower <= upper && vertex_compare(&data[lower], pivot) < 0) {
       lower++;
@@ -54,35 +56,33 @@ void quicksort(off_t bottom, off_t top, vertex_t *restrict data) {
   quicksort(upper + 1, top, data);
 }
 
-
-int
-main(int argc, char *argv[]) {
-  //struct my3DVertexStruct array[MAXARRAY];
-  struct my3DVertexStruct *array = (struct my3DVertexStruct *) malloc(MAXARRAY*sizeof(struct my3DVertexStruct)); // ED: let's allocate this monster on the heap, rather on the stack
+int main(int argc, char *argv[]) {
+  // vertex_t array[MAXARRAY];
+  vertex_t *array = (vertex_t *)malloc(
+      MAXARRAY * sizeof(vertex_t)); // ED: let's allocate this monster on
+                                    // the heap, rather on the stack
 
   FILE *fp;
-  int i,count=0;
+  int i, count = 0;
   int x, y, z;
-  
-  if (argc<2) {
-    fprintf(stderr,"Usage: qsort_large <file>\n");
+
+  if (argc < 2) {
+    fprintf(stderr, "Usage: qsort_large <file>\n");
     exit(-1);
-  }
-  else {
-    fp = fopen(argv[1],"r");
-    
-    while((fscanf(fp, "%d", &x) == 1) && (fscanf(fp, "%d", &y) == 1) && (fscanf(fp, "%d", &z) == 1) &&  (count < MAXARRAY)) {
-	 array[count].x = x;
-	 array[count].y = y;
-	 array[count].z = z;
-	 array[count].distance = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-	 count++;
+  } else {
+    fp = fopen(argv[1], "r");
+
+    while ((fscanf(fp, "%d", &x) == 1) && (fscanf(fp, "%d", &y) == 1) &&
+           (fscanf(fp, "%d", &z) == 1) && (count < MAXARRAY)) {
+      array[count].x = x;
+      array[count].y = y;
+      array[count].z = z;
+      array[count].distance = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+      count++;
     }
   }
-  printf("\nSorting %d vectors based on distance from the origin.\n\n",count);
+  printf("\nSorting %d vectors based on distance from the origin.\n\n", count);
   quicksort(0, count, array);
-  
-  for(i=0;i<count;i++)
-    printf("%d %d %d\n", array[i].x, array[i].y, array[i].z);
+
   return 0;
 }
